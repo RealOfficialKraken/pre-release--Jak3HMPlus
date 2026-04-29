19b2f4176f04825ac075b2152eb66f4b68cd9d99
Matt <146060745+RealOfficialKraken@users.noreply.github.com>
2026-04-29 02:37:51 -0500

    load of fixes

--- decompiler/level_extractor/extract_level.cpp
+++ decompiler/level_extractor/extract_level.cpp
@@ -358,7 +358,7 @@ void extract_common(const ObjectFileDB& db,
   extract_single_ag("VOCX.DGO", "flamer-lava-ag", db, tex_db, tfrag_level);
   extract_single_ag("WCA.DGO", "kanga-lizard-ag", db, tex_db, tfrag_level);
   extract_single_ag("DESBATTL.DGO", "mh-flyer-ag", db, tex_db, tfrag_level);
-  extract_single_ag("NSA.DGO", "mh-bat-ag", db, tex_db, tfrag_level);
+  extract_single_ag("LNSTOA.DGO", "mh-bat-ag", db, tex_db, tfrag_level);
 
 
   // copy in any art groups that were requested to be common
